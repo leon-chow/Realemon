ce93b0b4b257074e4f3250617ae8a241ac9b0486
Leon <leon.chow1@ontariotechu.net>
2020-08-05 16:46:34 -0400

    Init commit, add Character class, add combat class

---
+++ Character.java
@@ -0,0 +1,39 @@
+class Character {
+    public String job;    
+    public int HP;
+    public int MP;
+    public int attack;
+    public int defence;
+    public int magicDefence;
+    public int magicAttack;
+
+    public Character() {
+        this.job = '';
+        this.HP = 50;
+        this.MP = 25;
+        this.attack = 0;
+        this.defence = 0;
+        this.magicAttack = 0;
+        this.magicDefence = 0;
+    }
+
+    public void attack() {
+        
+    }
+
+    public void defend() {
+
+    }
+
+    public void jobChange() {
+
+    }
+
+    public Character createCharacter() {
+
+    }
+
+    public void heal() {
+        
+    }
+}
