23914c04254c7d5e73ac24a2f557241a3a7b40f9
Leon <leon.chow1@ontariotechu.net>
2020-08-05 22:05:51 -0400

    Add main menu options with user input

--- realemon/Main.java
+++ realemon/Main.java
@@ -1,11 +1,29 @@
 package realemon;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Real-e-mon! Please type in a number for one " + 
+        "of the following options! \n 1. Load a character. \n 2. Create a new character." +
+        "\n 3. Delete a character. \n 4. Exit");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-        Character character = new Character("Leon", "Jobless", 50, 50, 50, 50, 50, 50);
+        String choice = myObj.nextLine();  // Read user input
         
-        System.out.println(character.name);
+        while (true) {
+            if (Integer.parseInt(choice.trim()) == 1) {
+                System.out.println("Choose a character to load.");
+            } else if (Integer.parseInt(choice.trim()) == 2) {
+                System.out.println("Creating a new character...");
+            } else if (Integer.parseInt(choice.trim()) == 3) {
+                System.out.println("Choose a character to delete.");
+            } else if (Integer.parseInt(choice.trim()) == 4) {
+                System.out.println("Goodbye...");
+                System.exit(1);
+            } else {
+                System.out.println("Invalid option. ");
+                choice = myObj.nextLine();
+            }
+        }
     }
 }
